05996822df40ed28de64f83da449a3d6f2c10641
shmilee <shmilee.zju@gmail.com>
2024-09-08 08:47:00 +0800

    add, test SSH_FLAGS, --ssh-set-flags, SSH_BIN etc.

--- atb.sh
+++ atb.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env bash
+# Copyright (c) 2024 shmilee
+# Copyright (c) 2013-2024 Laurent Cozic (laurent22)
+# License: MIT
+#
 
 APPNAME=$(basename "$0" | sed "s/\.sh$//")
 
 # ---------------------------------------------------------------------------
 # Log functions
 # ---------------------------------------------------------------------------
-
-fn_log_info()  { echo "${APPNAME}: $1"; }
-fn_log_warn()  { echo "${APPNAME^^}: [WARNING] $1" 1>&2; }
-fn_log_error() { echo "${APPNAME^^}: [ERROR] $1" 1>&2; }
+fn_log_info()  { echo "[${APPNAME}] $1"; }
+fn_log_warn()  { echo "[${APPNAME^^}] WARNING $1" 1>&2; }
+fn_log_error() { echo "[${APPNAME^^}] ERROR $1" 1>&2; }
 fn_log_info_cmd()  {
     if [ -n "$SSH_DEST_FOLDER_PREFIX" ]; then
-        echo "${APPNAME^}: $SSH_CMD '$1'";
+        echo "[${APPNAME^}] $SSH_CMD '$1'";
     else
-        echo "${APPNAME^}: $1";
+        echo "[${APPNAME^}] $1";
     fi
 }
 
 # ---------------------------------------------------------------------------
 # Make sure everything really stops when CTRL+C is pressed
 # ---------------------------------------------------------------------------
-
-fn_terminate_script() {
-    fn_log_info "SIGINT caught."
-    exit 1
-}
-
+fn_terminate_script() { fn_log_info "SIGINT caught."; exit 1; }
 trap 'fn_terminate_script' SIGINT
 
 # ---------------------------------------------------------------------------
@@ -90,7 +88,6 @@ fn_expire_backup() {
         fn_log_error "$1 is not on a backup destination - aborting."
         exit 1
     fi
-
     fn_log_info "Expiring $1"
     fn_rm_dir "$1"
 }
@@ -98,72 +95,56 @@ fn_expire_backup() {
 fn_expire_backups() {
     local current_timestamp=$EPOCH
     local last_kept_timestamp=9999999999
-
     # we will keep requested backup
     backup_to_keep="$1"
     # we will also keep the oldest backup
     oldest_backup_to_keep="$(fn_find_backups | sort | sed -n '1p')"
-
     # Process each backup dir from the oldest to the most recent
     for backup_dir in $(fn_find_backups | sort); do
-
         local backup_date=$(basename "$backup_dir")
         local backup_timestamp=$(fn_parse_date "$backup_date")
-
         # Skip if failed to parse date...
         if [ -z "$backup_timestamp" ]; then
             fn_log_warn "Could not parse date: $backup_dir"
             continue
         fi
-
         if [ "$backup_dir" == "$backup_to_keep" ]; then
             # this is the latest backup requsted to be kept. We can finish pruning
             break
         fi
-
         if [ "$backup_dir" == "$oldest_backup_to_keep" ]; then
             # We dont't want to delete the oldest backup. It becomes first "last kept" backup
             last_kept_timestamp=$backup_timestamp
             # As we keep it we can skip processing it and go to the next oldest one in the loop
             continue
         fi
-
         # Find which strategy token applies to this particular backup
         for strategy_token in $(echo $EXPIRATION_STRATEGY | tr " " "\n" | sort -r -n); do
             IFS=':' read -r -a t <<< "$strategy_token"
-
             # After which date (relative to today) this token applies (X) - we use seconds to get exact cut off time
             local cut_off_timestamp=$((current_timestamp - ${t[0]} * 86400))
-
             # Every how many days should a backup be kept past the cut off date (Y) - we use days (not seconds)
             local cut_off_interval_days=$((${t[1]}))
-
             # If we've found the strategy token that applies to this backup
             if [ "$backup_timestamp" -le "$cut_off_timestamp" ]; then
-
                 # Special case: if Y is "0" we delete every time
                 if [ $cut_off_interval_days -eq "0" ]; then
                     fn_expire_backup "$backup_dir"
                     break
                 fi
-
                 # we calculate days number since last kept backup
                 local last_kept_timestamp_days=$((last_kept_timestamp / 86400))
                 local backup_timestamp_days=$((backup_timestamp / 86400))
                 local interval_since_last_kept_days=$((backup_timestamp_days - last_kept_timestamp_days))
-
                 # Check if the current backup is in the interval between
                 # the last backup that was kept and Y
                 # to determine what to keep/delete we use days difference
                 if [ "$interval_since_last_kept_days" -lt "$cut_off_interval_days" ]; then
-
                     # Yes: Delete that one
                     fn_expire_backup "$backup_dir"
                     # backup deleted no point to check shorter timespan strategies - go to the next backup
                     break
-
                 else
-
                     # No: Keep it.
                     # this is now the last kept backup
                     last_kept_timestamp=$backup_timestamp
@@ -177,6 +158,10 @@ fn_expire_backups() {
 
 fn_parse_profile(){
     local PRF="$1"
+    if [ ! -f "${PRF}" ]; then
+        fn_log_error "Profile not found: '${PRF}'!"
+        exit 2
+    fi
     local fname="$(basename "${PRF}")"
     local N0=$(awk '/FILTER_RULES_BEGIN/{print NR;exit}' "${PRF}")
     local N1=$(awk '/^[ ]*FILTER_RULES_BEGIN[ ]*$/{print NR;exit}' "${PRF}")
@@ -205,34 +190,36 @@ fn_parse_profile(){
 
 fn_parse_ssh() {
     # To keep compatibility with bash version < 3, we use grep
-    if echo "$DEST_FOLDER"|grep -Eq '^[A-Za-z0-9\._%\+\-]+@[A-Za-z0-9.\-]+:.+$'
-    then
+    if echo "$DEST_FOLDER" | grep -Eq '^[A-Za-z0-9\._%\+\-]+@[A-Za-z0-9.\-]+:.+$'; then
         SSH_USER=$(echo "$DEST_FOLDER" | sed -E  's/^([A-Za-z0-9\._%\+\-]+)@([A-Za-z0-9.\-]+)\:(.+)$/\1/')
         SSH_HOST=$(echo "$DEST_FOLDER" | sed -E  's/^([A-Za-z0-9\._%\+\-]+)@([A-Za-z0-9.\-]+)\:(.+)$/\2/')
         SSH_DEST_FOLDER=$(echo "$DEST_FOLDER" | sed -E  's/^([A-Za-z0-9\._%\+\-]+)@([A-Za-z0-9.\-]+)\:(.+)$/\3/')
-        if [ -n "$ID_RSA" ] ; then
-            SSH_CMD="ssh -p $SSH_PORT -i $ID_RSA ${SSH_USER}@${SSH_HOST}"
-        else
-            SSH_CMD="ssh -p $SSH_PORT ${SSH_USER}@${SSH_HOST}"
-        fi
+        SSH_CMD="$SSH_BIN $SSH_FLAGS ${SSH_USER}@${SSH_HOST}"
         SSH_DEST_FOLDER_PREFIX="${SSH_USER}@${SSH_HOST}:"
-    elif echo "$SRC_FOLDER"|grep -Eq '^[A-Za-z0-9\._%\+\-]+@[A-Za-z0-9.\-]+:.+$'
-    then
+    elif echo "$SRC_FOLDER" | grep -Eq '^[A-Za-z0-9\._%\+\-]+@[A-Za-z0-9.\-]+:.+$'; then
         SSH_USER=$(echo "$SRC_FOLDER" | sed -E  's/^([A-Za-z0-9\._%\+\-]+)@([A-Za-z0-9.\-]+)\:(.+)$/\1/')
         SSH_HOST=$(echo "$SRC_FOLDER" | sed -E  's/^([A-Za-z0-9\._%\+\-]+)@([A-Za-z0-9.\-]+)\:(.+)$/\2/')
         SSH_SRC_FOLDER=$(echo "$SRC_FOLDER" | sed -E  's/^([A-Za-z0-9\._%\+\-]+)@([A-Za-z0-9.\-]+)\:(.+)$/\3/')
-        if [ -n "$ID_RSA" ] ; then
-            SSH_CMD="ssh -p $SSH_PORT -i $ID_RSA ${SSH_USER}@${SSH_HOST}"
-        else
-            SSH_CMD="ssh -p $SSH_PORT ${SSH_USER}@${SSH_HOST}"
-        fi
+        SSH_CMD="$SSH_BIN $SSH_FLAGS ${SSH_USER}@${SSH_HOST}"
         SSH_SRC_FOLDER_PREFIX="${SSH_USER}@${SSH_HOST}:"
     fi
 }
 
+fn_check_BIN() {
+    if [ -n "$SSH_CMD" ]; then
+        if ! hash "$SSH_BIN" &>/dev/null; then
+            fn_log_error "Command not found: '$SSH_BIN'!"
+            exit 3
+        fi
+    fi
+    if ! hash "$RSYNC_BIN" &>/dev/null; then
+        fn_log_error "Command not found: '$RSYNC_BIN'!"
+        exit 3
+    fi
+}
+
 fn_run_cmd() {
-    if [ -n "$SSH_DEST_FOLDER_PREFIX" ]
-    then
+    if [ -n "$SSH_DEST_FOLDER_PREFIX" ]; then
         eval "$SSH_CMD '$1'"
     else
         eval $1
@@ -240,54 +227,23 @@ fn_run_cmd() {
 }
 
 fn_run_cmd_src() {
-    if [ -n "$SSH_SRC_FOLDER_PREFIX" ]
-    then
+    if [ -n "$SSH_SRC_FOLDER_PREFIX" ]; then
         eval "$SSH_CMD '$1'"
     else
         eval $1
     fi
 }
 
-fn_find() {
-    fn_run_cmd "find '$1'"  2>/dev/null
-}
-
-fn_get_absolute_path() {
-    fn_run_cmd "cd '$1';pwd"
-}
-
-fn_mkdir() {
-    fn_run_cmd "mkdir -p -- '$1'"
-}
-
-# Removes a file or symlink - not for directories
-fn_rm_file() {
-    fn_run_cmd "rm -f -- '$1'"
-}
-
-fn_rm_dir() {
-    fn_run_cmd "rm -rf -- '$1'"
-}
-
-fn_touch() {
-    fn_run_cmd "touch -- '$1'"
-}
-
-fn_ln() {
-    fn_run_cmd "ln -s -- '$1' '$2'"
-}
-
-fn_test_file_exists_src() {
-    fn_run_cmd_src "test -e '$1'"
-}
-
-fn_df_t_src() {
-    fn_run_cmd_src "df -T '${1}/'"
-}
-
-fn_df_t() {
-    fn_run_cmd "df -T '${1}/'"
-}
+fn_find() { fn_run_cmd "find '$1'"  2>/dev/null; }
+fn_get_absolute_path() { fn_run_cmd "cd '$1'; pwd"; }
+fn_mkdir() { fn_run_cmd "mkdir -p -- '$1'"; }
+fn_rm_file() { fn_run_cmd "rm -f -- '$1'"; } # rm a file,symlink - not dir
+fn_rm_dir() { fn_run_cmd "rm -rf -- '$1'"; }
+fn_touch() { fn_run_cmd "touch -- '$1'"; }
+fn_ln() { fn_run_cmd "ln -s -- '$1' '$2'"; }
+fn_test_file_exists_src() { fn_run_cmd_src "test -e '$1'"; }
+fn_df_t_src() { fn_run_cmd_src "df -T '${1}/'"; }
+fn_df_t() { fn_run_cmd "df -T '${1}/'"; }
 
 # ---------------------------------------------------------------------------
 # Source and destination information
@@ -296,11 +252,11 @@ SSH_USER=""
 SSH_HOST=""
 SSH_DEST_FOLDER=""
 SSH_SRC_FOLDER=""
-SSH_CMD=""
 SSH_DEST_FOLDER_PREFIX=""
 SSH_SRC_FOLDER_PREFIX=""
-SSH_PORT="22"
-ID_RSA=""
+SSH_BIN="ssh"
+SSH_FLAGS="-o ServerAliveInterval=60"
+SSH_CMD=""
 
 SRC_FOLDER=""
 DEST_FOLDER=""
@@ -310,6 +266,7 @@ AUTO_DELETE_LOG="1"
 EXPIRATION_STRATEGY="1:1 30:7 365:30"
 AUTO_EXPIRE="1"
 
+RSYNC_BIN="rsync"
 RSYNC_FLAGS="-D --numeric-ids --links --hard-links --one-file-system --itemize-changes --times --recursive --perms --owner --group --stats --human-readable"
 
 while :; do
@@ -321,6 +278,19 @@ while :; do
         -p|--profile)
             shift
             fn_parse_profile "$1"
+            ;;
+        --ssh-get-flags)
+            shift
+            echo "$SSH_FLAGS"
+            exit
+            ;;
+        --ssh-set-flags)
+            shift
+            SSH_FLAGS="$1"
+            ;;
+        --ssh-append-flags)
+            shift
+            SSH_FLAGS="$SSH_FLAGS $1"
             ;;
         --rsync-get-flags)
             shift
@@ -399,6 +369,7 @@ EOF
 DEST_FOLDER="${DEST_FOLDER%/}"
 
 fn_parse_ssh
+fn_check_BIN
 
 if [ -n "$SSH_DEST_FOLDER" ]; then
     DEST_FOLDER="$SSH_DEST_FOLDER"
@@ -427,7 +398,6 @@ done
 # ---------------------------------------------------------------------------
 # Check that the destination drive is a backup drive
 # ---------------------------------------------------------------------------
-
 # TODO: check that the destination supports hard links
 
 fn_backup_marker_path() { echo "$1/backup.marker"; }
@@ -445,7 +415,6 @@ fi
 # Check source and destination file-system (df -T /dest).
 # If one of them is FAT, use the --modify-window rsync parameter
 # (see man rsync) with a value of 1 or 2.
-#
 # The check is performed by taking the second row
 # of the output of the first command.
 if [[ "$(fn_df_t_src "${SRC_FOLDER}" | awk '{print $2}' | grep -c -i -e "fat")" -gt 0 ]]; then
@@ -461,7 +430,6 @@ fi
 # ---------------------------------------------------------------------------
 # Setup additional variables
 # ---------------------------------------------------------------------------
-
 # Date logic
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 EPOCH=$(date "+%s")
@@ -477,7 +445,6 @@ MYPID="$$"
 # ---------------------------------------------------------------------------
 # Create log folder if it doesn't exist
 # ---------------------------------------------------------------------------
-
 if [ ! -d "$LOG_DIR" ]; then
     fn_log_info "Creating log folder in '$LOG_DIR'..."
     mkdir -- "$LOG_DIR"
@@ -486,18 +453,14 @@ fi
 # ---------------------------------------------------------------------------
 # Handle case where a previous backup failed or was interrupted.
 # ---------------------------------------------------------------------------
-
 if [ -n "$(fn_find "$INPROGRESS_FILE")" ]; then
     if [ "$OSTYPE" == "cygwin" ]; then
         # 1. Grab the PID of previous run from the PID file
         RUNNINGPID="$(fn_run_cmd "cat $INPROGRESS_FILE")"
-
         # 2. Get the command for the process currently running under that PID and look for our script name
         RUNNINGCMD="$(procps -wwfo cmd -p $RUNNINGPID --no-headers | grep "$APPNAME")"
-
         # 3. Grab the exit code from grep (0=found, 1=not found)
         GREPCODE=$?
-
         # 4. if found, assume backup is still running
         if [ "$GREPCODE" = 0 ]; then
             fn_log_error "Previous backup task is still active - aborting (command: $RUNNINGCMD)."
@@ -511,8 +474,7 @@ if [ -n "$(fn_find "$INPROGRESS_FILE")" ]; then
         fi
     else
         RUNNINGPID="$(fn_run_cmd "cat $INPROGRESS_FILE")"
-        if ps -p "$RUNNINGPID" -o command | grep "$APPNAME"
-        then
+        if ps -p "$RUNNINGPID" -o command | grep "$APPNAME"; then
             fn_log_error "Previous backup task is still active - aborting."
             exit 1
         fi
@@ -535,11 +497,9 @@ fi
 
 # Run in a loop to handle the "No space left on device" logic.
 while : ; do
-
     # -----------------------------------------------------------------------
     # Check if we are doing an incremental backup (if previous backup exists).
     # -----------------------------------------------------------------------
-
     LINK_DEST_OPTION=""
     if [ -z "$PREVIOUS_DEST" ]; then
         fn_log_info "No previous backup - creating new one."
@@ -552,18 +512,8 @@ while : ; do
     fi
 
     # -----------------------------------------------------------------------
-    # Create destination folder if it doesn't already exists
-    # -----------------------------------------------------------------------
-
-    if [ -z "$(fn_find "$DEST -type d" 2>/dev/null)" ]; then
-        fn_log_info "Creating destination $SSH_DEST_FOLDER_PREFIX$DEST"
-        fn_mkdir "$DEST"
-    fi
-
-    # -----------------------------------------------------------------------
     # Purge certain old backups before beginning new backup.
     # -----------------------------------------------------------------------
-
     if [ -n "$PREVIOUS_DEST" ]; then
         # regardless of expiry strategy keep backup used for --link-dest
         fn_expire_backups "$PREVIOUS_DEST"
@@ -575,33 +525,29 @@ while : ; do
     # -----------------------------------------------------------------------
     # Start backup
     # -----------------------------------------------------------------------
-
     LOG_FILE="$LOG_DIR/$(date +"%Y-%m-%d-%H%M%S").log"
-
-    fn_log_info "Starting backup..."
-    fn_log_info "From: $SSH_SRC_FOLDER_PREFIX$SRC_FOLDER/"
-    fn_log_info "To:   $SSH_DEST_FOLDER_PREFIX$DEST/"
-
-    CMD="rsync"
+    CMD="$RSYNC_BIN $RSYNC_FLAGS --log-file '$LOG_FILE'"
     if [ -n "$SSH_CMD" ]; then
         RSYNC_FLAGS="$RSYNC_FLAGS --compress"
-        if [ -n "$ID_RSA" ] ; then
-            CMD="$CMD  -e 'ssh -p $SSH_PORT -i $ID_RSA -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
-        else
-            CMD="$CMD  -e 'ssh -p $SSH_PORT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
-        fi
+        CMD="$CMD -e '$SSH_BIN $SSH_FLAGS'"
     fi
-    CMD="$CMD $RSYNC_FLAGS"
-    CMD="$CMD --log-file '$LOG_FILE'"
     if [ -f "$FILTER_RULES" ]; then
         CMD="$CMD --filter='merge $FILTER_RULES'"
     fi
     CMD="$CMD $LINK_DEST_OPTION"
     CMD="$CMD -- '$SSH_SRC_FOLDER_PREFIX$SRC_FOLDER/' '$SSH_DEST_FOLDER_PREFIX$DEST/'"
-
+    fn_log_info "Starting backup ..."
+    fn_log_info "From: $SSH_SRC_FOLDER_PREFIX$SRC_FOLDER/"
+    fn_log_info "To:   $SSH_DEST_FOLDER_PREFIX$DEST/"
     fn_log_info "Running command:"
-    fn_log_info "$CMD"
+    fn_log_info "  $CMD"
     echo
+
+    # Create destination folder if it doesn't already exists
+    if [ -z "$(fn_find "$DEST -type d" 2>/dev/null)" ]; then
+        fn_log_info "Creating destination $SSH_DEST_FOLDER_PREFIX$DEST"
+        fn_mkdir "$DEST" || exit $?
+    fi
 
     fn_run_cmd "echo $MYPID > $INPROGRESS_FILE"
     eval $CMD
@@ -613,25 +559,18 @@ while : ; do
     # -----------------------------------------------------------------------
     # Check if we ran out of space
     # -----------------------------------------------------------------------
-
     NO_SPACE_LEFT="$(grep "No space left on device (28)\|Result too large (34)" "$LOG_FILE")"
-
     if [ -n "$NO_SPACE_LEFT" ]; then
-
         if [[ $AUTO_EXPIRE == "0" ]]; then
             fn_log_error "No space left on device, and automatic purging of old backups is disabled."
             exit 1
         fi
-
         fn_log_warn "No space left on device - removing oldest backup and resuming."
-
         if [[ "$(fn_find_backups | wc -l)" -lt "2" ]]; then
             fn_log_error "No space left on device, and no old backup to delete."
             exit 1
         fi
-
         fn_expire_backup "$(fn_find_backups | tail -n 1)"
-
         # Resume backup
         continue
     fi
@@ -639,7 +578,6 @@ while : ; do
     # -----------------------------------------------------------------------
     # Check whether rsync reported any errors
     # -----------------------------------------------------------------------
-
     EXIT_CODE="1"
     if [ -n "$(grep "rsync error:" "$LOG_FILE")" ]; then
         fn_log_error "Rsync reported an error, backup failed."
@@ -666,7 +604,6 @@ while : ; do
         # Create the latest symlink only when rsync succeeded
         fn_rm_file "$DEST_FOLDER/latest"
         fn_ln "$(basename -- "$DEST")" "$DEST_FOLDER/latest"
-
         # Remove .inprogress file only when rsync succeeded
         fn_rm_file "$INPROGRESS_FILE"
     fi
